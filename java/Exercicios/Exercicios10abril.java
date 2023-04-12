15b28338dda26eea170636e99160558dd38d62a1
Ana Carolina Capistrano Huntemann <92680998+navestelar@users.noreply.github.com>
2023-04-11 22:11:14 -0300

    Add files via upload

--- java/Exercicios/Exercicios10abril.java
+++ java/Exercicios/Exercicios10abril.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Exercicios10abril {
@@ -9,13 +10,17 @@ public class Exercicios10abril {
         int codigo = 0;
         Scanner code = new Scanner(System.in);
         System.out.println("CÓDIGO     DESCRIÇÃO");
-        System.out.println("  1        números pares de 1 a 100");
-        System.out.println("  2        média aritmética de uma lista de números");
-        System.out.println("  3        número máximo");
-        System.out.println("  4        palíndromo");
-        System.out.println("  5        fatorial");
+        System.out.println("  1        Números pares de 1 a 100");
+        System.out.println("  2        Média aritmética de uma lista de números");
+        System.out.println("  3        Número máximo");
+        System.out.println("  4        Palíndromo");
+        System.out.println("  5        Fatorial");
         System.out.println("  6        Fibonacci");
         System.out.println("  7        Soma dos dígitos");
+        System.out.println("  8        Número primo");
+        System.out.println("  9        MDC");
+        System.out.println(" 10        MMC");
+        System.out.println(" 11        Soma Array");
         System.out.println("---------------------------------------------------");
         System.out.println("Escolha seu código: ");
         codigo = code.nextInt();
@@ -38,7 +43,16 @@ public class Exercicios10abril {
             }
             media /= qtdNumero;
             System.out.print("A média aritmética é: "+media);       
-        }else if (codigo==3){
+        }else if (codigo==21){
+            double[] numeros21 = {2.5,3.0,4.5,5.5};
+            double soma21 = 0;
+            for (double numero21 : numeros21){
+                soma21+=numero21;
+            }
+            double media21 = soma21/numeros21.length;
+            System.out.println("A média é: "+media21);
+        }
+        else if (codigo==3){
             double n1,n2 = 0;
             int qtdN = 0;
             Scanner input3 = new Scanner(System.in);
@@ -98,6 +112,95 @@ public class Exercicios10abril {
                 y6=z6;
                 System.out.println(x6);
             } 
+        }else if (codigo==7){
+            int n7 = 0;
+        int digito7 = 0;
+        int z7 = 0;
+        int soma7 = 0;
+        Scanner input7 = new Scanner(System.in);
+        System.out.print("Escreva um número inteiro: ");
+        n7 = input7.nextInt();
+        for (int i=(String.valueOf(n7).length()-1); i>=0; i--){
+            digito7 = (int) (n7/Math.pow(10, i)-z7);
+            z7 = z7*10 + digito7*10;
+            soma7 = soma7 + digito7;
+        }
+        System.out.println("A soma dos dígitos do número "+n7+" é "+soma7);
+        }else if (codigo==8){
+            int n8 = 0;
+        int contador8 = 0;
+        Scanner input8 = new Scanner(System.in);
+        System.out.print("Escreva um número inteiro: ");
+        n8 = input8.nextInt();
+        for (int i=1;i<=n8;i++){
+            if(n8%i==0){
+                contador8++;
+            }
+        }
+        if (contador8==2){
+            System.out.println("O número "+n8+" é primo");
+        }else {
+            System.out.println("O número "+n8+" não é primo.");
+        }
+        }else if (codigo==9){
+            int n91, n92, mdc9 = 0;
+            Scanner input91 = new Scanner(System.in);
+            System.out.print("Escreva o primeiro número inteiro: ");
+            n91 = input91.nextInt();
+            Scanner input92 = new Scanner(System.in);
+            System.out.print("Escreva o segundo número inteiro: ");
+            n92 = input92.nextInt();
+            for(int i9=1;i9<=n91;i9++){
+                if(n91%i9==0 && n92%i9==0){
+                    mdc9=i9;
+                }
+            }
+            System.out.println("O Máximo Divisor Comum entre "+n91+" e "+n92+" é igual a "+mdc9);
+        }else if(codigo==10){
+            int n101, n102, mmc10 = 0;
+            Scanner input101 = new Scanner(System.in);
+            System.out.print("Escreva o primeiro número inteiro: ");
+            n101 = input101.nextInt();
+            Scanner input102 = new Scanner(System.in);
+            System.out.print("Escreva o segundo número inteiro: ");
+            n102 = input102.nextInt();
+            for(int i10=n101*n102;i10>=2;i10--){
+                if(i10%n101==0 && i10%n102==0){
+                    mmc10=i10;
+                }
+            }
+            System.out.println("O Mínimo Múltiplo Comum entre "+n101+" e "+n102+" é igual a "+mmc10);
+        }else if (codigo==11){
+            int soma11 = 0;
+            Scanner input111 = new Scanner(System.in);
+            System.out.print("Escreva a quantidade de números: ");
+            int qtd11 = input111.nextInt();
+            int n11[] = new int[qtd11];
+            for (int i11 = 0; i11<qtd11; i11++){
+                Scanner input112 = new Scanner(System.in);
+                System.out.println("Escreva o "+(i11+1)+" número: ");
+                n11[i11] = input112.nextInt();
+                soma11 += n11[i11];
+            }
+            System.out.println("A soma do Array é: "+soma11);
+        }else if(codigo==111){
+            int[] numeros1111 = {1,2,3,4,5};
+            int soma1111 = 0;
+            for (int i1111 = 0; i1111<numeros1111.length;i1111++){
+             soma1111+=numeros1111[i1111];
+            } 
+            System.out.println("A soma do Array "+Arrays.toString(numeros1111)+" é: "+soma1111);
+        }else if(codigo==121){
+            int[] numeros12 = {5,3,9,2,1};
+            int menor12 = numeros12[0];
+            for(int i12=1;i12<numeros12.length;i12++){
+             if(numeros12[i12]<menor12){
+                 menor12 = numeros12[i12];
+             }
+            }
+            System.out.println("O menor valor do Array "+Arrays.toString(numeros12)+" é "+menor12);
+        }else if(codigo==131){
+            
         }
         else{
             System.out.println("Código inválido!");
