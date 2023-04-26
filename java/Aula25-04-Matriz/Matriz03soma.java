fcd71fbdb5ecf971a328e6db4ed728395c9c4dd6
Ana Carolina Capistrano Huntemann <92680998+navestelar@users.noreply.github.com>
2023-04-25 21:15:23 -0300

    Add files via upload

---
+++ java/Aula25-04-Matriz/Matriz03soma.java
@@ -0,0 +1,14 @@
+public class Matriz03soma {
+    public static void main (String[] args) {
+        int[][] matriz = {{1,2,3}, {4,5,6}, {7,8,9}};
+        int soma = 0;
+        //qtd de linhas
+        for(int i=0;i<matriz.length;i++){
+            //qtd de colunas
+            for(int j=0;j<matriz[0].length;j++){
+                soma+=matriz[i][j];
+            }
+        }
+        System.out.println("A soma dos elementos da matriz é: "+soma);
+    }
+}
