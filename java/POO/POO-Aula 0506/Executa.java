3183091564352ed6a112ecce8a75c51bdf02430d
navestelar <acch.escola@gmail.com>
2023-06-08 20:14:26 -0300

    array

--- java/POO/POO-Aula 0506/Executa.java
+++ java/POO/POO-Aula 0506/Executa.java
@@ -1,9 +1,10 @@
 public class Executa {
     public static void main(String[] args) {
         Contato email = new Email("email");
-        email.exibir();
-        Pessoa pessoa = new Pessoa("email", 5);
+        // email.exibir();
+        Pessoa pessoa = new Pessoa("nome", 5);
+        // System.out.println(pessoa.getNome());
         pessoa.adicionarContato(email);
-        System.out.println(pessoa.getClass());
+        pessoa.exibirContatos();
     }
 }
--- java/POO/POO-Aula 0506/Pessoa.java
+++ java/POO/POO-Aula 0506/Pessoa.java
@@ -1,20 +1,21 @@
 // import java.lang.reflect.Array;
 
 public class Pessoa {
-    private Contato contatos[];
+    public Contato[] contatos;
     private String nome;
     private int max;
-    private int cont=0;
+    private int cont;
 
     public Pessoa(String nome, int max){
         this.nome=nome;
         this.max=max;
+        this.contatos = new Contato[max];
     }
     
     public void adicionarContato(Contato c){
-        if (cont<max) {
-            this.contatos[cont]=c;
-            cont++;
+        if (this.cont<this.max){
+            this.contatos[cont] = c;
+            this.cont++;
         }
     }
     
@@ -22,50 +23,50 @@ public class Pessoa {
         return contatos;
     }
     
-    public Contato[] getContatos(String tipo){
-        int j;
-        int a;
-        // Contato getContatos[];
-        // if (tipo.toLowerCase()=="email"){
-        //     for (int i=0; i<max; i++){
-        //         if (contatos[i].getTipo().toLowerCase()=="email"){
-        //             getContatos[j]=contatos[i];
-        //             j++;
-        //         }
-        //     }
-        // }else{
-        //     for (int i=0; i<max; i++){
-        //         if (contatos[i].getTipo().toLowerCase()=="telefone"){
-        //             getContatos[j]=contatos[i];
-        //             j++;
-        //         }
-        //     }
-        // }
-        return contatos;
-    }
+    // public Contato[] getContatos(String tipo){
+    //     // Contato getContatos[];
+    //     // if (tipo.toLowerCase()=="email"){
+    //     //     for (int i=0; i<max; i++){
+    //     //         if (contatos[i].getTipo().toLowerCase()=="email"){
+    //     //             getContatos[j]=contatos[i];
+    //     //             j++;
+    //     //         }
+    //     //     }
+    //     // }else{
+    //     //     for (int i=0; i<max; i++){
+    //     //         if (contatos[i].getTipo().toLowerCase()=="telefone"){
+    //     //             getContatos[j]=contatos[i];
+    //     //             j++;
+    //     //         }
+    //     //     }
+    //     // }
+    //     return contatos;
+    // }
 
-    public boolean possuiEmail(){
-        boolean possuiEmail;
-        if (this.getContatos("email").length>0){
-            possuiEmail=true;
-        } else {
-            possuiEmail=false;
-        }
-        return possuiEmail;
-    }
+    // public boolean possuiEmail(){
+    //     boolean possuiEmail;
+    //     if (this.getContatos("email").length>0){
+    //         possuiEmail=true;
+    //     } else {
+    //         possuiEmail=false;
+    //     }
+    //     return possuiEmail;
+    // }
     
-    public boolean possuiTelefone(){
-        boolean possuiTelefone;
-        if (this.getContatos("telefone").length>0){
-            possuiTelefone=true;
-        } else {
-            possuiTelefone=false;
-        }
-        return possuiTelefone;
-    }
+    // public boolean possuiTelefone(){
+    //     boolean possuiTelefone;
+    //     if (this.getContatos("telefone").length>0){
+    //         possuiTelefone=true;
+    //     } else {
+    //         possuiTelefone=false;
+    //     }
+    //     return possuiTelefone;
+    // }
     
     public void exibirContatos(){
-        
+        for (Contato contato : contatos){
+            contato.exibir();
+        }
     }
 
     public String getNome() {
